57e017e603512112e948e89169c552128a3e5426
sergigpec <100777782+Sergigpec@users.noreply.github.com>
2025-05-28 13:59:07 +0200

    feat: add https://the-italian-gold-garden.onrender.com in allowedOrigins

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://127.0.0.1:5501',
     'http://localhost:5500',
-    'http://localhost:5501'
+    'http://localhost:5501',
+    'https://the-italian-gold-garden.onrender.com'
 ];
 
 
